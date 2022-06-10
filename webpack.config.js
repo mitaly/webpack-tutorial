977072002953675d3ebc0e5b61bed3b5e52e53c6
mitaly <senmitaly13@gmail.com>
2022-06-10 23:01:45 +0530

    added md5 contenthash in file names

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: 'dist/'
     },
@@ -53,7 +53,7 @@ module.exports = {
     plugins: [
         new TerserPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'style.css'
+            filename: 'style[contenthash].css'
         })
     ]
 };
